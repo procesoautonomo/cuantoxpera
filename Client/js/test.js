19f2337d90228833e4d1f5239e63f41bc1b514c4
Matis <marguello@procesoautonomo.com>
2021-04-12 19:53:41 -0300

    cambiamos de lugar los datos del acelerometro

--- Client/js/test.js
+++ Client/js/test.js
@@ -6,36 +6,34 @@ $(document).ready(function () {
     console.log("Acceleration along the Y-axis " + acl.y);
     console.log("Acceleration along the Z-axis " + acl.z);
     
-    $("#ul_personas").empty();
+    $("#ul_acc").empty();
 
     var li_X =
-      '<li class="flex items-center justify-between bg-orange-primary text-sm text-white sm:text-base placeholder-gray-500 pl-1 pr-4 rounded-lg border-2 border-white w-full py-2 focus:outline-none mb-3">\
+      '<li>\
             <div> Acceleration along the X-axis ' +
       acl.x +
       "\
             </div>\
         </li>";
-    $("#ul_personas").append(li_X);
+    $("#ul_acc").append(li_X);
 
     var li_Y =
-      '<li class="flex items-center justify-between bg-orange-primary text-sm text-white sm:text-base placeholder-gray-500 pl-1 pr-4 rounded-lg border-2 border-white w-full py-2 focus:outline-none mb-3">\
-        <div> Acceleration along the X-axis ' +
+      '<li>\
+        <div> Acceleration along the Y-axis ' +
       acl.y +
       "\
         </div>\
     </li>";
-    $("#ul_personas").append(li_Y);
+    $("#ul_acc").append(li_Y);
 
     var li_Z =
-      '<li class="flex items-center justify-between bg-orange-primary text-sm text-white sm:text-base placeholder-gray-500 pl-1 pr-4 rounded-lg border-2 border-white w-full py-2 focus:outline-none mb-3">\
-            <div> Acceleration along the X-axis ' +
+      '<li>\
+            <div> Acceleration along the Z-axis ' +
       acl.z +
       "\
             </div>\
         </li>";
-    $("#ul_personas").append(li_Z);
-
-    $("#panel_personas").show();
+    $("#ul_acc").append(li_Z);
   });
 
   acl.start();
