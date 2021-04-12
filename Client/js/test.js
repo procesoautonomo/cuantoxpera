9433bba467ff11c2068f3c3fe277ed3a97788ba7
Matis <marguello@procesoautonomo.com>
2021-04-12 19:41:14 -0300

    testeamos acelerómetro

---
+++ Client/js/test.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  let acl = new Accelerometer({ frequency: 60 });
+
+  acl.addEventListener("reading", () => {
+    console.log("Acceleration along the X-axis " + acl.x);
+    console.log("Acceleration along the Y-axis " + acl.y);
+    console.log("Acceleration along the Z-axis " + acl.z);
+
+    var li_X =
+      '<li class="flex items-center justify-between bg-orange-primary text-sm text-white sm:text-base placeholder-gray-500 pl-1 pr-4 rounded-lg border-2 border-white w-full py-2 focus:outline-none mb-3">\
+            <div> Acceleration along the X-axis ' +
+      acl.x +
+      "\
+            </div>\
+        </li>";
+    $("#ul_personas").append(li_X);
+
+    var li_Y =
+      '<li class="flex items-center justify-between bg-orange-primary text-sm text-white sm:text-base placeholder-gray-500 pl-1 pr-4 rounded-lg border-2 border-white w-full py-2 focus:outline-none mb-3">\
+        <div> Acceleration along the X-axis ' +
+      acl.y +
+      "\
+        </div>\
+    </li>";
+    $("#ul_personas").append(li_Y);
+
+    var li_Z =
+      '<li class="flex items-center justify-between bg-orange-primary text-sm text-white sm:text-base placeholder-gray-500 pl-1 pr-4 rounded-lg border-2 border-white w-full py-2 focus:outline-none mb-3">\
+            <div> Acceleration along the X-axis ' +
+      acl.z +
+      "\
+            </div>\
+        </li>";
+    $("#ul_personas").append(li_Z);
+
+    $("#panel_personas").show();
+  });
+
+  acl.start();
+});
