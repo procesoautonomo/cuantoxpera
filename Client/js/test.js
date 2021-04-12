f5c842db3db5919f29608c47c2ef84d4ccddf1db
Matis <marguello@procesoautonomo.com>
2021-04-12 20:33:41 -0300

    cambiamos fondo con parametros acelerometro

--- Client/js/test.js
+++ Client/js/test.js
@@ -6,25 +6,33 @@ $(document).ready(function () {
     console.log("Acceleration along the Y-axis " + acl.y);
     console.log("Acceleration along the Z-axis " + acl.z);
 
-    $("#ul_acc").empty();
+    var x = Math.round(map(Math.abs(acl.x.toPrecision(2)), 0, 10, 0, 255));
+    var y = Math.round(map(Math.abs(acl.y.toPrecision(2)), 0, 10, 0, 255));
+    var z = Math.round(map(Math.abs(acl.z.toPrecision(2)), 0, 10, 0, 255));
 
     var li_X = "<li>\
-            <div> Acceleration along the X-axis " + map(Math.abs(acl.x.toPrecision(2)),0,9.9,0,255) + "\
+            <div> Acceleration along the X-axis " + x + "\
             </div>\
         </li>";
-    $("#ul_acc").append(li_X);
-
+    
     var li_Y = "<li>\
-        <div> Acceleration along the Y-axis " + map(Math.abs(acl.y.toPrecision(2)),0,9.9,0,255) + "\
+        <div> Acceleration along the Y-axis " + y + "\
         </div>\
     </li>";
-    $("#ul_acc").append(li_Y);
 
     var li_Z = "<li>\
-            <div> Acceleration along the Z-axis " + map(Math.abs(acl.z.toPrecision(2)),0,9.9,0,255) + "\
+            <div> Acceleration along the Z-axis " + z + "\
             </div>\
         </li>";
+
+    $("#ul_acc").empty();
+    $("#ul_acc").append(li_X);
+    $("#ul_acc").append(li_Y);
     $("#ul_acc").append(li_Z);
+
+    var thergb = "rgb(" + x + "," + y + "," + z + ")";
+    $("#ul_acc").css( "background-color", thergb );
+    
   });
 
   acl.start();
@@ -40,8 +48,8 @@ function map(n, start1, stop1, start2, stop2, withinBounds) {
     return newval;
   }
   if (start2 < stop2) {
-    return this.constrain(newval, start2, stop2);
+    return constrain(newval, start2, stop2);
   } else {
-    return this.constrain(newval, stop2, start2);
+    return constrain(newval, stop2, start2);
   }
 }
