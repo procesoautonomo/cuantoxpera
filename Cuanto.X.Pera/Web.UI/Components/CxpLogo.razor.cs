c2ec79a4a70f1a393056f24d32bcc6076549167c
Matis <marguello@procesoautonomo.com>
2021-05-17 00:44:40 -0300

    Creando Index y aplicando diseño figma con variantes

---
+++ Cuanto.X.Pera/Web.UI/Components/CxpLogo.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.UI.Components
+{
+    public partial class CxpLogo : ComponentBase
+    {
+        [Parameter] public bool IsSmallVersion { get; set; } = false;
+    }
+}
